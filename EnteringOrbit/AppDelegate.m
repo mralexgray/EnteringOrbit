607664fe1c345b5ce1128d276c2b65583856c915
Toru <toru.inoue@kissaki.tv>
2013-09-24 09:18:23 +0900

    update method

--- EnteringOrbit/AppDelegate.m
+++ EnteringOrbit/AppDelegate.m
@@ -11,6 +11,9 @@
 #import "KSMessenger.h"
 #import "WebSocketClientController.h"
 
+#define KEY_LIMITSEC    (@"-l")
+#define KEY_TARGET      (@"-t")
+
 
 @implementation AppDelegate {
     NSDictionary * paramDict;
@@ -21,33 +24,86 @@
 
 - (id) initAppDelegateWithParam:(NSDictionary * )dict {
     if (self = [super init]) {
-        paramDict = [[NSDictionary alloc]initWithDictionary:dict];
+//        paramDict = [[NSDictionary alloc]initWithDictionary:dict];
+        paramDict = @{KEY_TARGET:@"mondogrosso@mondogrosso.201104392.members.btmm.icloud.com"};
     }
     return self;
 }
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    NSFileHandle * fHand = [NSFileHandle fileHandleForWritingAtPath:@"/Users/highvision/Desktop/testing/test.txt"];
-    NSAssert(fHand, @"nil");
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    
+
+    NSFileManager * fileManager = [NSFileManager defaultManager];
+    NSError * error;
+    
+    NSString * tempLogFilePath = [[NSString alloc]initWithFormat:@"%@/%@", [[NSBundle mainBundle] bundlePath], @"temp.txt"];
+    
+    // if exist, delete
+    NSFileHandle * readHandle = [NSFileHandle fileHandleForReadingAtPath:tempLogFilePath];
+    if (readHandle) {// delete
+        [fileManager removeItemAtPath:tempLogFilePath error:&error];
+    }
+    
+    
+    // create temporary output
+    bool makeTempFile = [fileManager createFileAtPath:tempLogFilePath contents:nil attributes:nil];
+    NSAssert1(makeTempFile, @"failed to make tempfile @ %@, please chech chmod", tempLogFilePath);
+    
+    NSFileHandle * outhand = [NSFileHandle fileHandleForWritingAtPath:tempLogFilePath];
+    NSAssert(outhand, @"nil");
+    
+
+    
     /**
      NSTaskでsshで繋いでlogがあるところまで移動してログの内容をtailしてWebSocketで出力する。
      プロセスの開始時にWebSocketクライアントになる。
      */
+    NSString * expect = @"/usr/bin/expect";
+    
+//    http://www.math.kobe-u.ac.jp/~kodama/tips-expect.html
+    
+//    expect
+//    -c "set timeout 30;
+//    spawn ssh mondogrosso@mondogrosso.201104392.members.btmm.icloud.com;
+//    send \"tail -f ./Desktop/130715_2_テロップ.txt\n\";
+//    interact;"
+    
+    NSString * cHead = @"-c";
+    
+    int timeLimitSec = 30;
+    if (paramDict[KEY_LIMITSEC]) timeLimitSec = [paramDict[KEY_LIMITSEC] intValue];
+    NSString * timeLimitPhrase = [[NSString alloc] initWithFormat:@"set timeout %d;", timeLimitSec];
+    
+    NSAssert1(paramDict[KEY_TARGET], @"target required. %@ targetUserName@targetMachineName", KEY_TARGET);
+    
+    NSString * connectTarget = [[NSString alloc]initWithFormat:@"spawn ssh %@;" ,paramDict[KEY_TARGET]];
     
     
-    NSArray * paramArray = @[@"-l", @"sassembla", @"192.168.11.6"];
-    //    NSArray * paramArray = @[@"-2", @"-6", @"mondogrosso@mondogrosso.201104392.members.btmm.icloud.com"];
+    /*
+     combine lines
+     */
+    NSArray * expectParamArray = @[timeLimitPhrase,
+                                   connectTarget,
+                                   @"send \"tail -f ./Desktop/testLog.txt\n\";",
+                                   @"interact;"];
     
+    NSArray * paramArray = @[cHead, [expectParamArray componentsJoinedByString:@"\n"]];
+
     NSPipe * pipeFromSSH = [[NSPipe alloc]init];
     
     NSTask * ssh = [[NSTask alloc]init];
-    [ssh setLaunchPath:@"/usr/bin/ssh"];
+    [ssh setLaunchPath:expect];
     [ssh setArguments:paramArray];
-    [ssh setStandardOutput:fHand];
+    
+    [ssh setStandardOutput:outhand];
+    [ssh setStandardError:outhand];
+    
+    NSFileHandle * fileHandleForReading = [pipeFromSSH fileHandleForReading];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(update:) name:nil object:nil];
+    
     [ssh launch];
     
-    NSLog(@"over");
     
     
     
@@ -59,9 +115,19 @@
     //    [tail setStandardInput:pipeFromSSH];
     //
     //    [tail launch];
+}
+
+- (void) update:(NSNotification * )update {
+    if ([update name] == NSApplicationWillUpdateNotification) return;
+    if ([update name] == NSTextInputContextKeyboardSelectionDidChangeNotification) return;
+    if ([update name] == NSApplicationDidUpdateNotification) return;
+    if ([update name] == NSApplicationDidBecomeActiveNotification) return;
+    if ([update name] == NSApplicationWillBecomeActiveNotification) return;
+    if ([update name] == NSApplicationDidResignActiveNotification) return;
+    if ([update name] == NSApplicationWillResignActiveNotification) return;
     
     
-    
+    NSLog(@"update %@", update);//もしかしたらキャッチできるかもしれない。
 }
 
 @end
